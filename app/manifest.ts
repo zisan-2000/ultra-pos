dfd2e70ffaaba5df4cbf82f2a2bd87998737a3c6
BoedZisan <stack1@birdsofeden.me>
2026-01-20 18:09:06 +0600

    manifest josn updated !

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,7 +7,7 @@ export default function manifest(): MetadataRoute.Manifest {
     id: "/dashboard",
     start_url: "/dashboard",
     display: "standalone",
-    background_color: "#ffffff",
+    background_color: "#000000",
     theme_color: "#000000",
     icons: [
       {
